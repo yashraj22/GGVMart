1dbdf4d7a44ea1294ae505d435d2b3001f4eb141
Gaurav Kumar <indiagauravkumar@gmail.com>
2024-05-12 14:08:57 +0530

    cleanup in homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useUserAuth } from "./context/AuthContext";
 import Products from "./components/Products";
 
 const HomePage = () => {
-  const { user, loginWithGoogle, logOut }: any = useUserAuth();
+  const { user }: any = useUserAuth();
 
   const [chats, setChats] = useState([]);
 
@@ -40,64 +40,16 @@ const HomePage = () => {
     console.log(user);
   }, [user]);
 
-  const handleLogout = async () => {
-    try {
-      await logOut();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   if (user) {
-    // Display user's name and avatar if login is successful
     return (
-      <div>
-        {/* <h1>Posts</h1>
-        <div>
-          <p>Welcome, {user.user_metadata?.full_name || ""}!</p>
-          <Image
-            width={50}
-            height={50}
-            src={user.user_metadata?.picture || ""}
-            alt="User avatar"
-          />
-        </div>
-        <button onClick={handleLogout}>LogOut</button> */}
-        {/* <ChatUi /> */}
-
-        {/* {chats && (
-          <>
-            {" "}
-            <h2>Chats:</h2>
-            <ul>
-              {chats.map((chat: any) => (
-                <li className="bg-red-300 my-2" key={chat.id}>
-                  <p>Chat ID: {chat.id}</p>
-                  <p>User ID: {chat.userId}</p>
-                  <p>Product ID: {chat.productId}</p>
-                 
-                </li>
-              ))}
-            </ul>
-          </>
-        )} */}
-
-        {/* <ProductForm /> */}
+      <>
+        <div className="container mx-auto max-w-7xl flex items-center justify-between p-4"></div>
         <Products />
-        {/* <p>My Products</p>
-        <MyProducts /> */}
-      </div>
+      </>
     );
   }
 
-  // If not logged in, show the login button
-  return (
-    <div>
-      {/* <h1>Posts</h1>
-      <button onClick={loginWithGoogle}>Sign in with Google</button>
-      <button>My Products</button> */}
-    </div>
-  );
+  return <div></div>;
 };
 
 const Home = () => {

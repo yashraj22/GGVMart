311d6c827b26ede55262b2a54ded757d87d84c26
yashraj22 <yyashraj05@gmail.com>
2024-06-04 13:38:24 +0530

    feat: Fetch and display user data in UserCard component

--- app/components/UserCard.tsx
+++ app/components/UserCard.tsx
@@ -1,13 +1,39 @@
 import Image from "next/image";
-import { useUserAuth } from "../context/AuthContext";
+import { useEffect, useState } from "react";
 
-const UserCard = () => {
-  const { user }: any = useUserAuth();
+const UserCard = ({ prop, chatId, onUserCardClick }: any) => {
+  const [userData, setuserData] = useState<any>();
+
+  useEffect(() => {
+    const fetchUserDetail = async () => {
+      try {
+        const response = await fetch(`/api/user/${prop}`);
+        const data = await response.json();
+        setuserData(data);
+        console.log("userdata");
+        console.log(data);
+      } catch (error) {
+        console.error("Failed to fetch product details:", error);
+      }
+    };
+
+    if (prop) {
+      fetchUserDetail();
+    }
+  }, [prop]);
+
+  const handleClick = () => {
+    onUserCardClick(chatId, prop);
+  };
+
   return (
-    <div className="flex h-20 w-full bg-slate-100 border border-gray-300 border-y-1 border-x-0">
+    <div
+      className="flex h-20 w-full bg-slate-100 border border-gray-300 border-y-1 border-x-0"
+      onClick={handleClick}
+    >
       <div className="flex h-20 ml-4 items-center ">
         <Image
-          src={user?.user_metadata?.picture}
+          src={userData && userData.user.raw_user_meta_data.picture}
           alt="User Avatar"
           width={50}
           height={50}
@@ -15,7 +41,7 @@ const UserCard = () => {
         />
       </div>
       <div className="flex mt-5 ml-4">
-        <h1>Gaurav Kumar</h1>
+        <h1>{userData && userData.user.raw_user_meta_data.name}</h1>
       </div>
     </div>
   );

cb6691e44fe9b76115330d0eb4a11354fa418774
yashraj22 <yyashraj05@gmail.com>
2024-06-04 13:46:24 +0530

    chore: Remove unused code in MyChats component

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -4,51 +4,7 @@ import MyChatScreen from "../components/MyChatScreen";
 import { useUserAuth } from "../context/AuthContext";
 
 const MyChats = () => {
-  const [chats, setChats] = useState();
-  const [Loading, setLoading] = useState(true);
-  const { user }: any = useUserAuth();
-  const [products, setProducts] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchChats = async () => {
-  //     try {
-  //       const response = await fetch("/api/chats");
-  //       const data = await response.json();
-  //       setChats(data);
-  //     } catch (error) {
-  //       console.error("Failed to fetch chats:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-  //   fetchChats();
-  // }, []);
-
-  useEffect(() => {
-    if (user) {
-      const fetchProducts = async () => {
-        try {
-          const response = await fetch(
-            `/api/product/myproduct/${user.identities[0].user_id}`,
-          );
-          const data = await response.json();
-          setProducts(data.products); // Assuming 'data.products' is the array
-        } catch (error) {
-          console.error("Failed to fetch products:", error);
-        }
-      };
-
-      fetchProducts();
-    }
-  }, [user]);
-
-  const getOwnerId = () => {
-    if (products) {
-      return products[0].ownerId;
-    }
-  };
-
-  return <MyChatScreen prop={getOwnerId} />;
+  return <MyChatScreen />;
 };
 
 export default MyChats;

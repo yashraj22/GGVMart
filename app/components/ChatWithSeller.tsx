f05ee639cb160cddbf6b0718103be88fc56868d3
yashraj22 <yyashraj05@gmail.com>
2024-06-14 19:43:05 +0530

    chore: Update navigate import in ChatWithSeller component

--- app/components/ChatWithSeller.tsx
+++ app/components/ChatWithSeller.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useUserAuth } from "@/app/context/AuthContext";
-import { navigate } from "../util/actions";
+import { navigate } from "../util/redirect";
 import supabase from "../util/supabaseClient";
 
 const ChatWithSeller = ({ productId, receiverId }: any) => {

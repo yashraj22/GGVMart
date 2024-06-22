3365525dc16ce4d09dd47d7ec2ff5fdc758c1684
yashraj22 <yyashraj05@gmail.com>
2024-06-22 08:17:25 +0530

    chore: Update purgeStore action to clear and flush storage immediately after logout

--- app/util/actions.ts
+++ app/util/actions.ts
@@ -1,6 +1,9 @@
 import { PURGE } from "redux-persist";
+import { persistor } from "../redux/store/store";
 
 export const purgeStore = () => {
+  persistor.purge(); // optional: clear storage if needed
+  persistor.flush(); // optional: flush storage to make sure state is persisted immediately
   return {
     type: PURGE,
     key: "root", // This should match the key you used in persistConfig
